8cfc503bd49e9939df1ef9dfb7dca39961b8dc36
Zac Olden <zacolden@gmail.com>
2020-04-26 01:46:59 +1000

    Clean up crime & gym pages
    Use prepared statements, fix undefined index/offset errors

--- public/gym.php
+++ public/gym.php
@@ -3,60 +3,63 @@ include 'header.php';
 
 $checkeffects = DB::run("SELECT * FROM `effects` WHERE `userid`='$user_class->id'");
 $numeffects = $checkeffects->rowCount();
-if($numeffects > 0){
+
+if ($numeffects > 0) {
 	echo Message("You can't train at the gym if you have an effect.");
 	include 'footer.php';
 	die();
 }
 
-if($user_class->jail > 0){
+if ($user_class->jail > 0) {
 	echo Message("You can't train at the gym if you are in jail.");
 	include 'footer.php';
 	die();
 }
 
-if($user_class->hospital > 0){
+if ($user_class->hospital > 0) {
 	echo Message("You can't train at the gym if you are in the hospital.");
 	include 'footer.php';
 	die();
 }
 
-if ($_POST['train'] != "") {
+if (isset($_POST['train'])) {
 
- if ($_POST['energy'] > $user_class->energy){
-  echo Message("You don't have that much energy.");
-  include "footer.php";
-  die();
-}
+    if ($_POST['energy'] > $user_class->energy) {
+      echo Message("You don't have that much energy.");
+      include "footer.php";
+      die();
+    }
 
-if ($_POST['energy'] < 1){
-  echo Message("Please enter a valid amount.");
-  include "footer.php";
-  die();
-}
-	if($_POST['type'] == 1){ // strength
-		$newstrength = $user_class->strength + floor($_POST['energy'] * ($user_class->awake / 100));
-		$result =  DB::run("UPDATE `grpgusers` SET `strength` = '".$newstrength."' WHERE `id` = '".$user_class->id."'");
-		echo Message("You trained with ".$_POST['energy']." energy and recieved ".floor($_POST['energy'] * ($user_class->awake / 100))." strength.");
-	}elseif($_POST['type'] == 2){ // defense
-		$newdefense = $user_class->defense + floor($_POST['energy'] * ($user_class->awake / 100));
-		$result =  DB::run("UPDATE `grpgusers` SET `defense` = '".$newdefense."' WHERE `id` = '".$user_class->id."'");
-		echo Message("You trained with ".$_POST['energy']." energy and recieved ".floor($_POST['energy'] * ($user_class->awake / 100))." defense.");
-	}elseif($_POST['type'] == 3){ // speed
-		$newspeed = $user_class->speed + floor($_POST['energy'] * ($user_class->awake / 100));
-		$result =  DB::run("UPDATE `grpgusers` SET `speed` = '".$newspeed."' WHERE `id` = '".$user_class->id."'");
-		echo Message("You trained with ".$_POST['energy']." energy and recieved ".floor($_POST['energy'] * ($user_class->awake / 100))." speed.");
-	}
+    if ($_POST['energy'] < 1) {
+      echo Message("Please enter a valid amount.");
+      include "footer.php";
+      die();
+    }
 
-$newawake = $user_class->awake - (2 * $_POST['energy']);
-if ($newawake <0 ){
-	$newawake = 0;
-}
-$newenergy = $user_class->energy - $_POST['energy'];
+    if($_POST['type'] == 1) { // strength
+        $newstrength = $user_class->strength + floor($_POST['energy'] * ($user_class->awake / 100));
+        $result =  DB::run("UPDATE `grpgusers` SET `strength` = ? WHERE `id` = ?", [$newstrength, $user_class->id]);
+        echo Message("You trained with ".$_POST['energy']." energy and recieved ".floor($_POST['energy'] * ($user_class->awake / 100))." strength.");
+    } elseif($_POST['type'] == 2) { // defense
+        $newdefense = $user_class->defense + floor($_POST['energy'] * ($user_class->awake / 100));
+        $result =  DB::run("UPDATE `grpgusers` SET `defense` = ? WHERE `id` = ?", [$newdefense, $user_class->id]);
+        echo Message("You trained with ".$_POST['energy']." energy and recieved ".floor($_POST['energy'] * ($user_class->awake / 100))." defense.");
+    } elseif($_POST['type'] == 3){ // speed
+        $newspeed = $user_class->speed + floor($_POST['energy'] * ($user_class->awake / 100));
+        $result =  DB::run("UPDATE `grpgusers` SET `speed` = ? WHERE `id` = ?", [$newspeed, $user_class->id]);
+        echo Message("You trained with ".$_POST['energy']." energy and recieved ".floor($_POST['energy'] * ($user_class->awake / 100))." speed.");
+    }
 
-$result =  DB::run("UPDATE `grpgusers` SET `awake` = '".$newawake."', `energy` = '".$newenergy."' WHERE `id` = '".$user_class->id."'");
+    $newawake = $user_class->awake - (2 * $_POST['energy']);
 
-$user_class = new User($_SESSION['id']);
+    if ($newawake < 0) {
+        $newawake = 0;
+    }
+    $newenergy = $user_class->energy - $_POST['energy'];
+
+    $result =  DB::run("UPDATE `grpgusers` SET `awake` = ?, `energy` = ? WHERE `id` = ?", [$newawake, $newenergy, $user_class->id]);
+
+    $user_class = new User($_SESSION['id']);
 }
 ?>
 	<tr><td class="contenthead">Gym</td></tr>
@@ -76,16 +79,13 @@ $user_class = new User($_SESSION['id']);
 	<tr><td class='contentcontent'>
 	<table width='100%'>
 	<tr>
-
 		<td width='15%'>Strength:</td>
 		<td width='35%'><?php echo $user_class->strength; ?></td>
 		<td width='15%'>Defense:</td>
 		<td width='35%'><?php echo $user_class->defense; ?></td>
 	</tr>
-
 	<tr>
 		<td width='15%'>Speed:</td>
-
 		<td width='35%'><?php echo $user_class->speed; ?></td>
 		<td width='15%'>Total:</td>
 		<td width='35%'><?php echo $user_class->totalattrib; ?></td>
